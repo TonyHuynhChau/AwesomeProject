e75823b069770143ff878318bb979415a3ddc31e
ThanhDat0810 <86997326+ThanhDat0810@users.noreply.github.com>
2022-02-27 17:46:34 +0700

    Create sever.js
    sever (not complete)

---
+++ sever.js
@@ -0,0 +1,9 @@
+const express = require('express');
+var app = express();
+
+app.post('register' , (req, res, next)=>{
+
+})
+app.listen(8000, ()  =>{
+    console.log('Server started on port');
+})
