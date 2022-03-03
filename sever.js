57b4e7360ac556dfad299ce140627218a1c08ed0
ThanhDat0810 <86997326+ThanhDat0810@users.noreply.github.com>
2022-03-03 19:14:40 +0700

    Server Not ServerAPI ( NODE MONGOOSE)

--- sever.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-var app = express();
-
-app.post('register' , (req, res, next)=>{
-
-})
-app.listen(8000, ()  =>{
-    console.log('Server started on port');
-})
