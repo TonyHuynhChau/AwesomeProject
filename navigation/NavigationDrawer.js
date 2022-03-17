b0c6d86ade95063f5aae33bc6ff85cdb90744f26
Shigeru21 <86502645+Shigeru21@users.noreply.github.com>
2022-03-17 09:39:12 +0700

    Add files via upload

---
+++ navigation/NavigationDrawer.js
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import { View, Text, Image, TouchableOpacity, Button } from 'react-native';
+
+import HomeScreen from './screens/HomeScreen';
+import SettingScreen from './screens/SettingScreen'
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+function Profile() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Profile</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+const CustomDrawer = props => {
+  return (
+    <View style={{ flex: 1 }}>
+      <DrawerContentScrollView {...props}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: 20,
+            backgroundColor: "#d64b4b",
+            marginBottom: 20,
+          }}
+        >
+          <View>
+            <Text>Shiba Shigeru</Text>
+              <Text>Bvinhshikeru@gmail.com</Text>
+          </View>
+     <Image source={require("./assets/pc.png")} 
+      style={{ width: 60, height: 60, borderRadius: 30 }}
+     />
+        </View>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          right: 0,
+          left: 0,
+          bottom: 50,
+          backgroundColor: "#d64b4b",
+          padding: 20,
+        }}
+      >
+        <Text>Log Out</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: 'transparent',
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        headerTitle: '',
+      }}
+      drawerContent={props => <CustomDrawer {...props} />}
+    >
+      <Drawer.Screen component={HomeScreen} name='Home' />
+      <Drawer.Screen component={SettingScreen} name=' Setting' /> 
+      <Drawer.Screen component={Profile} name='Profile' />
+    </Drawer.Navigator>
+  );
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <DrawerNavigator />
+    </NavigationContainer>
+  );
+}
